2c23bd083a6fdee793fe4a7f8243da6f79b782d8
Paula Nicole Guevara <123211853+Paula-n-guevara@users.noreply.github.com>
2024-11-04 20:48:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Tooltip translation effect
-    const translatableElements = document.querySelectorAll(".translatable");
-    translatableElements.forEach(element => {
-        element.addEventListener("mouseenter", () => {
-            const translation = element.getAttribute("data-translation");
-            const tooltip = document.createElement("div");
-            tooltip.classList.add("tooltip");
-            tooltip.innerText = translation;
-            element.appendChild(tooltip);
-        });
-
-        element.addEventListener("mouseleave", () => {
-            const tooltip = element.querySelector(".tooltip");
-            if (tooltip) {
-                tooltip.remove();
-            }
-        });
-    });
-document.addEventListener("DOMContentLoaded", () => {
-    // Tooltip translation effect
-    const translatableElements = document.querySelectorAll(".translatable");
-    translatableElements.forEach(element => {
-        element.addEventListener("mouseenter", () => {
-            const translation = element.getAttribute("data-translation");
-            const tooltip = document.createElement("div");
-            tooltip.classList.add("tooltip");
-            tooltip.innerText = translation;
-            element.appendChild(tooltip);
-        });
-
-        element.addEventListener("mouseleave", () => {
-            const tooltip = element.querySelector(".tooltip");
-            if (tooltip) {
-                tooltip.remove();
-            }
-        });
-    });
-document.addEventListener("DOMContentLoaded", () => {
     const counters = document.querySelectorAll(".counter");
 
     counters.forEach(counter => {
